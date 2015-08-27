373484d736cde924fd9488b3a9b059ca9dd2b89a
Carlos Rodriguez <crodriguez@redborder.net>
2015-08-27 13:32:55 +0200

    Added Composition type

---
+++ src/main/java/net/redborder/utils/generators/types/CompositionType.java
@@ -0,0 +1,33 @@
+package net.redborder.utils.generators.types;
+
+import com.google.common.base.Joiner;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class CompositionType implements Type {
+    private String separator;
+    private List<Type> components;
+
+    public CompositionType(Map<String, Object> params) {
+        this.separator = (String) params.get("separator");
+        List<Map<String, Object>> componentsList = (List<Map<String, Object>>) params.get("components");
+
+        for (Map<String, Object> componentMap : componentsList) {
+            Type componentType = TypeManager.newType(componentMap);
+            components.add(componentType);
+        }
+    }
+
+    @Override
+    public Object get() {
+        List<String> componentsResults = new ArrayList<>();
+
+        for (Type type : components) {
+            componentsResults.add(String.valueOf(type.get()));
+        }
+
+        return Joiner.on(separator).join(componentsResults);
+    }
+}
