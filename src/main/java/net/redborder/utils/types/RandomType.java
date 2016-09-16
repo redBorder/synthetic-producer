ae4e8fa9b750acb142f832a28ebff424fd501234
Andres Gomez Ferrer <andresgomezfrr@gmail.com>
2016-09-16 10:13:33 +0200

    Random type improvment

--- src/main/java/net/redborder/utils/types/RandomType.java
+++ src/main/java/net/redborder/utils/types/RandomType.java
@@ -3,19 +3,28 @@ package net.redborder.utils.types;
 import java.util.*;
 
 public class RandomType implements Type {
-    List<Type> types = new ArrayList<>();
+    List<String> typesName = new ArrayList<>();
+    Map<String, Type> types = new HashMap<>();
     Random r = new Random();
 
     public RandomType(Map<String, Object> params) {
         Map<String, Map<String, Object>> components = (Map<String, Map<String, Object>>) params.get("components");
 
         for (Map.Entry<String, Map<String, Object>> component : components.entrySet()) {
-            types.add(TypeManager.newType(component.getValue()));
+            Type type = TypeManager.newType(component.getValue());
+            typesName.add(component.getKey());
+            types.put(component.getKey(), type);
         }
     }
 
     @Override
     public Object get() {
-        return types.get(r.nextInt(types.size())).get();
+        String typeName = typesName.get(r.nextInt(typesName.size()));
+        Type type = types.get(typeName);
+
+        Map<String, Object> typeValue = new HashMap<>();
+        typeValue.put(typeName, type.get());
+
+        return typeValue;
     }
 }
