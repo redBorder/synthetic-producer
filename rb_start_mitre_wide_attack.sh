dc6d8424b7bcba8456bb3c22ea99822622e6e687
Luis Blanco <ljblanco@redborder.com>
2024-10-24 11:54:05 +0100

    fix restarting mitre in case not exist yet

--- rb_start_mitre_wide_attack.sh
+++ rb_start_mitre_wide_attack.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo 'Restarting mitre_wide_attack.py'
-screen -X -S mitre_wide_attack quit
+screen -X -S mitre_wide_attack quit 2>/dev/null || true
 screen -dmS mitre_wide_attack python3 mitre_wide_attack.py
