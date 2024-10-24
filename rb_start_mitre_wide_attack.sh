9db4b91890b004858c9796ff5f12b68ef9e9e21c
Luis Blanco <ljblanco@redborder.com>
2024-10-24 13:53:15 +0100

    adding comments about how to attach and deatach from screen

--- rb_start_mitre_wide_attack.sh
+++ rb_start_mitre_wide_attack.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# How to run this script:
+# Run it: ./rb_start_mitre_wide_attack.sh
+#
+# To view the running screen session:
+# - To watch and attach to screen: screen -r mitre_wide_attack
+# - Detach from screen: Ctrl+A then D
+
 echo 'Restarting mitre_wide_attack.py'
 screen -X -S mitre_wide_attack quit 2>/dev/null || true
 screen -dmS mitre_wide_attack python3 mitre_wide_attack.py
