76a157a3a707736b734152624b7519da144aaaa6
Luis Blanco <ljblanco@redborder.com>
2024-10-25 12:21:07 +0100

    python3.9 doc way

--- mitre_wide_attack.py
+++ mitre_wide_attack.py
@@ -7,6 +7,7 @@ def usage():
     The orden is left to right, corresponding on the column order of the orginal mitre matrix
     On start, this script will create an instace in screen named "mitre_wide_attack" and will repeat itself every 2 hours.
     '''
+    print(__doc__)
     exit()
 
 # Array of scripts to execute in sequence
@@ -47,13 +48,10 @@ def check_and_kill_process(script, command):
         check_process = os.popen(f'pgrep -f "{command}"').read()
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-f', '--fast', action='store_true', default=False, help='Run in fast mode')
-    parser.add_argument('-h', '--help', action='store_true', default=False, help='Show help message')
-    args = parser.parse_args()    
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('-f', '--fast', action='store_true', default=False, help='Run with no wait between incidents')
 
-    if args.help:
-        usage()
+    args = parser.parse_args()    
 
     while True:
         for script in SCRIPTS_PATH:
