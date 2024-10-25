e04362c1e4bcb2717615ca2f8cdc3313e41083e7
Luis Blanco <ljblanco@redborder.com>
2024-10-25 13:01:19 +0100

    naming assets

--- py/assets.py
+++ py/assets.py
@@ -1,15 +1,15 @@
 import random
 lan_devices = [
-    ("192.168.0.1", "00:1a:2b:3c:4d:5e", "Cisco Systems, Inc"),
-    ("192.168.0.10", "00:2b:3c:4d:5e:6f", "Cisco Systems, Inc"),
-    ("192.168.0.20", "00:3c:4d:5e:6f:7a", "Cisco Systems, Inc"),
-    ("192.168.0.30", "00:4d:5e:6f:7a:8b", "TP-Link Corp."),
-    ("192.168.0.100", "00:5e:6f:7a:8b:9c", "ASUSTek COMPUTER INC."),
-    ("192.168.0.110", "00:6f:7a:8b:9c:0d", "ASUSTek COMPUTER INC."),
-    ("192.168.3.10", "00:8b:9c:0d:1e:2f", "ASUSTek COMPUTER INC."),
-    ("192.168.3.11", "00:9c:0d:1e:2f:3a", "ASUSTek COMPUTER INC."),
-    ("192.168.3.12", "00:0d:1e:2f:3a:4b", "ASUSTek COMPUTER INC."),
-    ("192.168.3.13", "00:1e:3a:4b:ff:1a", "Dell Inc.")
+    ("192.168.0.1", "00:1a:2b:3c:4d:5e", "Cisco Systems, Inc"),       #Main Router
+    ("192.168.0.10", "00:2b:3c:4d:5e:6f", "Cisco Systems, Inc"),      #BaseRouter
+    ("192.168.0.20", "00:3c:4d:5e:6f:7a", "Cisco Systems, Inc"),      #LabsRouter
+    ("192.168.0.30", "00:4d:5e:6f:7a:8b", "TP-Link Corp."),           #RecreationalRouter
+    ("192.168.0.100", "00:5e:6f:7a:8b:9c", "ASUSTek COMPUTER INC."),  #WebServer
+    ("192.168.0.110", "00:6f:7a:8b:9c:0d", "ASUSTek COMPUTER INC."),  #StoreageServer
+    ("192.168.3.10", "00:8b:9c:0d:1e:2f", "ASUSTek COMPUTER INC."),   #PCAlicia
+    ("192.168.3.11", "00:9c:0d:1e:2f:3a", "ASUSTek COMPUTER INC."),   #PCBob
+    ("192.168.3.12", "00:0d:1e:2f:3a:4b", "ASUSTek COMPUTER INC."),   #PCCarlos
+    ("192.168.3.13", "00:1e:3a:4b:ff:1a", "Dell Inc.")                #BasePrinter
 ]
 
 def random_lan():
